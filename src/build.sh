311e1d2398574e6d18f4d090019912bb829a0e39
tclostio <twclostio@protonmail.com>
2020-12-01 17:01:28 -0800

    updating my build scrip AGAIN

--- src/build.sh
+++ src/build.sh
@@ -4,8 +4,8 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <input file>"
 else
     echo "[*] Compiling..."
-    gcc -c -ggdb $1 -o ../obj/$1.o
-    gcc -c -ggdb helpers.c -o ../obj/helpers.o
+    gcc -c -Wall -ggdb $1 -o ../obj/$1.o
+    gcc -c -Wall -ggdb helpers.c -o ../obj/helpers.o
 
     echo "[*] Linking..."
     gcc -Wall -ggdb ../obj/helpers.o ../obj/$1.o -o ../bin/$1.out
