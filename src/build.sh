b4b451d416062742077e5ac19828b62e5d039306
tclostio <twclostio@protonmail.com>
2020-12-01 16:58:50 -0800

    updating my build script

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "[*] Compiling..."
-gcc -c -ggdb $1 -o ../obj/$1.o
-gcc -c -ggdb helpers.c -o ../obj/helpers.o
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <input file>"
+else
+    echo "[*] Compiling..."
+    gcc -c -ggdb $1 -o ../obj/$1.o
+    gcc -c -ggdb helpers.c -o ../obj/helpers.o
 
-echo "[*] Linking..."
-gcc -Wall -ggdb ../obj/helpers.o ../obj/$1.o -o ../bin/$1.out
+    echo "[*] Linking..."
+    gcc -Wall -ggdb ../obj/helpers.o ../obj/$1.o -o ../bin/$1.out
 
-echo "[*] Done."
+    echo "[*] Done."
+fi
