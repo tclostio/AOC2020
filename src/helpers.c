4a145b78463d91a90cedc14c8ac3d9fdd9f0bb90
tclostio <twclostio@protonmail.com>
2020-12-02 17:24:12 -0800

    day2

--- src/helpers.c
+++ src/helpers.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int getlength(int);
 int LENGTH; // i know i know
@@ -12,7 +13,7 @@ int LENGTH; // i know i know
 /*
  * WARNING: You must still free the 'lines' allocated memory after use in your main routine.
  */
-int* readlines(char* filename)
+int *readlines(char *filename)
 {
     FILE *fp;
     int temp;
@@ -37,10 +38,48 @@ int* readlines(char* filename)
            index = count -1;
            *(lines+index) = temp;
         }
-
     }
 
     LENGTH = count; // I KNOW
+    return lines;
+}
+
+/*
+ * Christ what a mess.
+ */
+char **readCharLines(char *filename)
+{
+    FILE *fp = fopen(filename, "rb+");
+    char *lineBuf = NULL;
+    size_t i, n = 0;
+    ssize_t lineLength = 0;
+    size_t increment = 10;
+
+    char **lines = malloc(increment * sizeof(char**));
+
+    while ((lineLength = getline(&lineBuf, &n, fp)) != -1) {
+        if (i >= increment) {
+            increment += increment;
+
+            char **tmp = realloc(lines, increment * sizeof(char**));
+            if(!tmp) {
+                perror("realloc");
+                exit(1);
+            }
+
+            lines = tmp;
+        }
+        // remove \n
+        lineBuf[strcspn(lineBuf, "\n")] = 0;
+
+        *(lines + i) = malloc((lineLength + 1) * sizeof(char));
+
+        strcpy(*(lines + i), lineBuf);
+
+        i++;
+        LENGTH = i;
+    }
+
     return lines;
 }
 
---
+++ src/puzzle3.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <regex.h>
+#include <stdbool.h>
+
+char **readCharLines(char*);
+bool is_correct_v1(int, int, char, char*);
+bool is_correct_v2(int, int, char, char*);
+extern int LENGTH; //this is jank as hell but it's a quick hack to keep a dynamic array length handy. don't @ me
+
+int main(int argc, char **argv)
+{
+    bool result1, result2;
+    char x;
+    char **lines;
+    int min, max, total1, total2;
+    char *pwd, *t1, *t2;
+
+    lines = readCharLines("../assets/input3.txt");
+
+    for(int i = 0; i < LENGTH; i++) {
+        pwd = strchr(lines[i], ':');
+        if (pwd != NULL) pwd += 1;
+
+        t1 = strsep(&lines[i], ":");
+        min = atoi(strsep(&t1, "-"));
+
+        t2 = t1;
+        max = atoi(strsep(&t2, " "));
+        x = t2[0];
+
+        result1 = is_correct_v1(min, max, x, pwd);
+        if (result1) total1 += 1;
+
+        result2 = is_correct_v2(min, max, x, pwd);
+        if (result2) total2 += 1;
+    }
+
+    printf("Total1: %d\nTotal2: %d\n", total1, total2);
+
+    printf("Test!\n");
+    int test1 = 2;
+    int test2 = 9;
+    char test3 = 'c';
+    char *test4 = "ccccccccc";
+
+    bool testRes = is_correct_v2(test1, test2, test3, test4);
+
+    printf("%d\n", testRes);
+
+    free(lines);
+
+    return 0;
+}
+
+bool is_correct_v1(int min, int max, char x, char *pwd)
+{
+    int i;
+    for (i = 0; pwd[i]; pwd[i] == x ? i++ : *pwd++); // spooky pointer magic here
+
+    if (i >= min && i <= max) return true;
+    else return false;
+}
+
+bool is_correct_v2(int pos1, int pos2, char x, char *pwd)
+{
+    if (pwd[pos1-1] == x && pwd[pos2-1] == x) return false;
+    else if (pwd[pos1-1] == x) return true;
+    else if (pwd[pos2-1] == x) return true;
+    else return false;
+}
+
